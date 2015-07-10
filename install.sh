bd3cf018dd4dfe35651802a8b592483152045f81
Rana Singh <Rana.Singh@reedps.com>
2015-07-10 16:48:22 +0100

    initial import of tmux conf updated installer moved location of inputrc file

--- install.sh
+++ install.sh
@@ -1,4 +1,20 @@
-BASH_CFG_DIR="$HOME/.bash_configs"
+BASH_CFG_DIR=".bash_configs"
 
 echo "Backing up existing config files"
-source $BASH_CFG_DIR/backup.sh
+source ~/$BASH_CFG_DIR/backup.sh
+
+function relink {
+   cd ~
+   rm -f ~/.$1
+   ln -s ~/$BASH_CFG_DIR/$1 .$1
+}
+
+relink vimrc
+relink viminfo
+relink bash_profile
+relink bashrc
+relink bash_logout
+relink tmux
+relink inputrc
+
+echo done
